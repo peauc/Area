3b3cccba0ac674966ae3420702c670efb22d1ba0
Romain samuel <romain.samuel@epitech.eu>
2017-10-25 12:49:49 +0200

    Database setup

--- src/main/java/eu/epitech/DatabaseManager.java
+++ src/main/java/eu/epitech/DatabaseManager.java
@@ -1,8 +1,27 @@
 package eu.epitech;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
 
-	public DatabaseManager() {
+	Connection connection;
 
+	public DatabaseManager() throws ClassNotFoundException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		try {
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=area&password=djh7.HDi5332jczj");
+
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
 	}
 }
